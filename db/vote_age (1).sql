220474bd9cd99f7ade6fadcd55f9ca46218f7dbd
nagachen <birdann99@gmail.com>
2023-08-10 23:31:26 +0800

    update db

--- db/vote_age (1).sql
+++ db/vote_age (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-08-09 17:17:42
+-- 產生時間： 2023-08-10 17:29:46
 -- 伺服器版本： 10.4.28-MariaDB
 -- PHP 版本： 8.2.4
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `vote_age` (
   `id` int(10) UNSIGNED NOT NULL,
   `option_id` int(11) NOT NULL,
-  `age_level` varchar(11) NOT NULL,
+  `age_level` varchar(20) NOT NULL,
   `age_vote` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,11 +39,45 @@ CREATE TABLE `vote_age` (
 --
 
 INSERT INTO `vote_age` (`id`, `option_id`, `age_level`, `age_vote`) VALUES
-(1, 2, '0', 0),
-(2, 3, '0', 0),
-(3, 4, '0', 0),
-(5, 5, '0', 0),
-(6, 6, '0', 0);
+(1, 3, 'age19-24', 1),
+(2, 3, 'age18', 1),
+(3, 4, 'age18', 1),
+(4, 4, 'age18', 1),
+(5, 5, 'age18', 1),
+(6, 5, 'age19-24', 1),
+(7, 6, 'age19-24', 1),
+(8, 3, 'age45-64', 1),
+(9, 2, 'age25-29', 1),
+(10, 2, 'age25-29', 1),
+(11, 2, 'age25-29', 1),
+(12, 5, 'age65', 1),
+(13, 4, 'age65', 1),
+(14, 3, 'age30-44', 1),
+(15, 2, 'age30-44', 1),
+(16, 6, 'age19-24', 1),
+(17, 3, 'age25-29', 1),
+(18, 2, 'age45-64', 1),
+(19, 3, 'age25-29', 1),
+(20, 4, 'age30-44', 1),
+(21, 3, 'age25-29', 1),
+(22, 3, 'age25-29', 1),
+(23, 4, 'age25-29', 1),
+(24, 2, 'age30-44', 1),
+(25, 2, 'age25-29', 1),
+(26, 4, 'age65', 1),
+(27, 4, 'age30-44', 1),
+(28, 2, 'age30-44', 1),
+(29, 2, 'age65', 1),
+(30, 2, 'age30-44', 1),
+(31, 6, 'age30-44', 1),
+(32, 3, 'age45-64', 1),
+(33, 2, 'age45-64', 1),
+(34, 3, 'age30-44', 1),
+(35, 3, 'age19-24', 1),
+(36, 3, 'age19-24', 1),
+(37, 2, 'age25-29', 1),
+(38, 3, 'age30-44', 1),
+(39, 4, 'age25-29', 1);
 
 --
 -- 已傾印資料表的索引
@@ -63,7 +97,7 @@ ALTER TABLE `vote_age`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `vote_age`
 --
 ALTER TABLE `vote_age`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- db/vote_income.sql
+++ db/vote_income.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-08-09 17:18:59
+-- 產生時間： 2023-08-10 17:30:11
 -- 伺服器版本： 10.4.28-MariaDB
 -- PHP 版本： 8.2.4
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `vote_income` (
   `id` int(10) UNSIGNED NOT NULL,
   `option_id` int(11) NOT NULL,
-  `income_level` varchar(11) NOT NULL,
+  `income_level` varchar(20) NOT NULL,
   `income_vote` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,11 +39,45 @@ CREATE TABLE `vote_income` (
 --
 
 INSERT INTO `vote_income` (`id`, `option_id`, `income_level`, `income_vote`) VALUES
-(1, 2, '0', 0),
-(2, 3, '0', 0),
-(3, 4, '0', 0),
-(4, 5, '0', 0),
-(5, 6, '0', 0);
+(1, 3, 'income59-80', 1),
+(2, 3, 'income59-80', 1),
+(3, 4, 'income59-80', 1),
+(4, 4, 'income30', 1),
+(5, 5, 'income30', 1),
+(6, 5, 'income30', 1),
+(7, 6, 'income30', 1),
+(8, 3, 'income30', 1),
+(9, 2, 'income81-100', 1),
+(10, 2, 'income59-80', 1),
+(11, 2, 'income59-80', 1),
+(12, 5, 'income81-100', 1),
+(13, 4, 'income81-100', 1),
+(14, 3, 'income46-59', 1),
+(15, 2, 'income59-80', 1),
+(16, 6, 'income30-45', 1),
+(17, 3, 'income59-80', 1),
+(18, 2, 'income30-45', 1),
+(19, 3, 'income81-100', 1),
+(20, 4, 'income59-80', 1),
+(21, 3, 'income100', 1),
+(22, 3, 'income100', 1),
+(23, 4, 'income100', 1),
+(24, 2, 'income30-45', 1),
+(25, 2, 'income59-80', 1),
+(26, 4, 'income100', 1),
+(27, 4, 'income81-100', 1),
+(28, 2, 'income30-45', 1),
+(29, 2, 'income46-59', 1),
+(30, 2, 'income59-80', 1),
+(31, 6, 'income81-100', 1),
+(32, 3, 'income59-80', 1),
+(33, 2, 'income30', 1),
+(34, 3, 'income30-45', 1),
+(35, 3, 'income81-100', 1),
+(36, 3, 'income81-100', 1),
+(37, 2, 'income59-80', 1),
+(38, 3, 'income30-45', 1),
+(39, 4, 'income100', 1);
 
 --
 -- 已傾印資料表的索引
@@ -63,7 +97,7 @@ ALTER TABLE `vote_income`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `vote_income`
 --
 ALTER TABLE `vote_income`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
