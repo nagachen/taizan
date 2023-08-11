ac4cb3e207d46edfe5595d6bbfb58535a7e6a97b
nagachen <birdann99@gmail.com>
2023-08-11 15:23:59 +0800

    增加title:key的顯示

--- api/show_vote.php
+++ api/show_vote.php
@@ -34,9 +34,28 @@ foreach($income as $idx=>$row){
 
     }   
 }
+
+$sql="SELECT option_id, SUM(income_vote) as income_vote FROM vote_income GROUP BY option_id";
+$option=$Income->q($sql);
+//  dd($option);
+$optionData=array();
+foreach($option as $idx=>$row){
+    foreach($row as $key =>$value){
+            // dd($key);
+            // dd($value);
+        if($key=='option_id'){
+            $currentKey=$Vote->find($value)['title'];
+        }else{
+
+            $incomeData[$currentKey] = $value;
+        }
+
+    }   
+}
 $mergedArray = array_merge($ageData, $incomeData);
+$mergedArray = array_merge($mergedArray, $optionData);
  
 // dd($mergedArray);
-   echo json_encode($mergedArray);
+    echo json_encode($mergedArray);
 //  echo json_encode($incomeData);
 ?>
